a256295489fe87f336f7d56006dcded97aae2798
Nate Jones and Pema Geyleg <pair+nate+pema@casecommons.org>
2016-05-25 11:06:38 -0400

    Delete React test setup
    [#120132823]

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,3 @@
 'use strict'
-
 import React, { Component, DOM } from 'react'
 import ReactDOM from 'react-dom'
-import HelloReact from 'components/HelloReact'
-
-var node = document.getElementById('container')
-
-ReactDOM.render(React.createElement(HelloReact), node)
