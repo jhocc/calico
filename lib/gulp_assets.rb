a4d9a19d4bdad213082d940fbda88686607b32a0
Nate Jones and Pema Geyleg <pair+nate+pema@casecommons.org>
2016-05-25 16:09:54 -0400

    asset paths based on environments
    [#120132823]

--- lib/gulp_assets.rb
+++ lib/gulp_assets.rb
@@ -6,21 +6,20 @@ module GulpAssets
     MANIFEST_PATH = Rails.root.join('public', 'assets', 'rev-manifest.json').freeze
 
     def gulp_asset_path(name)
-      # is_dev_env = Casebook2::ENVIRONMENT == 'development'
-      # is_test_env = Casebook2::ENVIRONMENT == 'test'
+      is_dev_env = Rails.env == 'development'
+      is_test_env = Rails.env  == 'test'
 
-      # use_manifest = !(is_dev_env || is_test_env)
-      # use_gulp_server = is_dev_env
-      use_gulp_server = true
+      use_manifest = !(is_dev_env || is_test_env)
+      use_gulp_server = is_dev_env
 
-      # if use_manifest
-        # name = manifest[name]
-      # end
+      if use_manifest
+        name = manifest[name]
+      end
 
       if use_gulp_server
         uri = URI::HTTP.build(host: request.host, port: 4857)
-      # else
-        # uri = URI::Generic.build({})
+      else
+        uri = URI::Generic.build({})
       end
 
       uri.path = "/assets/#{name}"
