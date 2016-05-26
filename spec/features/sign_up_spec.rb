5af3d4d253198b92fdc870408a55de3788ae0e2d
Nate Jones and Pema Geyleg <pair+nate+pema@casecommons.org>
2016-05-26 14:20:52 -0400

    Feature test for user profile page
    [#120132823]

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -17,15 +17,21 @@ feature 'Sign up' do
     fill_in 'user[first_name]', with: 'foo'
     fill_in 'user[last_name]', with: 'baz'
     fill_in 'user[email]', with: 'foo.baz@test.com'
-    fill_in 'user[address]', with: '14 main st'
-    fill_in 'user[city]', with: 'Brooklyn'
-    select 'New York', from: 'user[state]'
-    fill_in 'user[zip_code]', with: '10010'
+    fill_in 'user[phone]', with: '9177187777'
+    fill_in 'user[addresses][street_address]', with: '14 main st'
+    fill_in 'user[addresses][city]', with: 'Brooklyn'
+    select 'New York', from: 'user[addresses][state]'
+    fill_in 'user[addresses][zip_code]', with: '10010'
     fill_in 'user[password]', with: 'Password123'
     fill_in 'user[password_confirmation]', with: 'Password123'
     click_button 'Save'
 
     expect(page).to have_content 'Dashboard'
     expect(page).to have_content 'Sign Out'
+
+    click_link 'My Profile'
+    expect(page).to have_content 'foo baz'
+    expect(page).to have_content '14 main st'
+    expect(page).to have_content '9177187777'
   end
 end
