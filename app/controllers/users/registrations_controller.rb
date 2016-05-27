d340ed85540f8175a0fa8fb9f2ca811f16fc79e2
Jimmy Chao and Pema Geyleg <pair+jchao+pema@casecommons.org>
2016-05-27 16:15:30 -0400

    Update registration to be able to update without current_password
    bypass password requirement when the update params does not include
    password
    
    [#120129471]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -36,14 +36,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+  def update_resource(resource, update_account_params)
+    if params[:current_password].present?
+      resource.update_with_password(update_account_params)
+    else
+      resource.update_attributes(update_account_params)
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(
       :sign_up,
       keys: [
-        :first_name, :last_name, :phone,
+        :first_name,
+        :last_name,
+        :phone,
+        :email,
         :addresses_attributes => [
           :street_address, :city, :state, :zip_code
         ]
@@ -53,15 +63,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: [
-        :first_name, :last_name, :phone,
-        :addresses_attributes => [
-          :street_address, :city, :state, :zip_code
-        ]
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name,
+      :last_name,
+      :phone,
+      :email,
+      :addresses_attributes => [
+        :street_address, :city, :state, :zip_code, :id
       ]
-    )
+    ])
+
+    if params[:user][:password].blank?
+      except_keys = [
+        :current_password,
+        :password,
+        :password_confirmation,
+      ]
+      devise_parameter_sanitizer.permit(:account_update, except: except_keys)
+    end
   end
 
   # The path used after sign up.
--- spec/features/edit_user_profile_spec.rb
+++ spec/features/edit_user_profile_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 feature 'Edit user profile' do
   scenario 'User can edit thier profile' do
-    user = FactoryGirl.create(:user, first_name: 'john', last_name: 'doe', email: 'test@casecommons.org')
+    user = FactoryGirl.create(:user,
+                              first_name: 'john',
+                              last_name: 'doe',
+                              email: 'test@casecommons.org',
+                              password: '123456',
+                              password_confirmation: '123456')
     user.addresses << FactoryGirl.create(:address)
     user.save
 
@@ -14,10 +19,10 @@ feature 'Edit user profile' do
 
     fill_in 'First Name', with: 'Johnny'
     fill_in 'Street Address', with: 'Localhost 34th street'
+    fill_in 'Zip Code', with: '10010'
 
     click_button 'Update'
 
-    visit root_path
     click_link 'My Profile'
 
     expect(page).to have_content 'Johnny'
