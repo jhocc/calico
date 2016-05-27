d023b82bb92c94065f1e3693a3a3b0b6f8b1528a
Jimmy Chao and Pema Geyleg <pair+jchao+pema@casecommons.org>
2016-05-27 10:02:56 -0400

    Refactor gulp_assets helper

--- lib/gulp_assets.rb
+++ lib/gulp_assets.rb
@@ -6,24 +6,23 @@ module GulpAssets
     MANIFEST_PATH = Rails.root.join('public', 'assets', 'rev-manifest.json').freeze
 
     def gulp_asset_path(name)
-      is_dev_env = Rails.env == 'development'
-      is_test_env = Rails.env  == 'test'
-
-      use_manifest = !(is_dev_env || is_test_env)
-      use_gulp_server = is_dev_env
-
-      if use_manifest
-        name = manifest[name]
-      end
-
-      if use_gulp_server
+      if Rails.env == 'development'
         uri = URI::HTTP.build(host: request.host, port: 4857)
       else
         uri = URI::Generic.build({})
       end
 
-      uri.path = "/assets/#{name}"
+      if use_manifest?
+        uri.path = "/assets/#{manifest[name]}"
+      else
+        uri.path = "/assets/#{name}"
+      end
+
       uri.to_s
+    end
+
+    def use_manifest?
+      Rails.env != 'development' && Rails.env != 'test'
     end
 
     def manifest
