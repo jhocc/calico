2ec61df4a70646718e4a3750a22c6614fa4b3831
Pema Geyleg <pair+pema@casecommons.org>
2016-05-30 22:56:52 -0400

    Puma configuration
    [#120132823]

---
+++ config/puma.rb
@@ -0,0 +1,19 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
