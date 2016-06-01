95f015019678249a585bb4992549c14b498a726c
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-01 17:46:00 -0400

    Add channels and channels_users to user model
    To allow access to the current users channels
    
    [#120618109]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
+  has_many :channels_users
+  has_many :channels, through: :channels_users
   has_many :addresses
   accepts_nested_attributes_for :addresses
 
