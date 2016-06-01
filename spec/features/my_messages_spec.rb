1cf749ad44b438640877d61ec9a1c8128665b4c9
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-01 17:47:19 -0400

    Spelling mistake in feature spec

--- spec/features/my_messages_spec.rb
+++ spec/features/my_messages_spec.rb
@@ -4,13 +4,13 @@ feature 'my messages' do
   scenario 'user can see a list of other users who have messaged them' do
     user_myself = FactoryGirl.create(:user, first_name: 'Me', last_name: 'And Myself')
 
-    philip_fry = FactoryGirl.create(:user, first_name: 'Phillip', last_name: 'Fry')
+    phillip_fry = FactoryGirl.create(:user, first_name: 'Phillip', last_name: 'Fry')
     turanga_leela = FactoryGirl.create(:user, first_name: 'Turanga', last_name: 'Leela')
     bender_rodriguez = FactoryGirl.create(:user, first_name: 'Bender', last_name: 'Rodriguez')
 
     login_as user_myself
 
-    [philip_fry, turanga_leela, bender_rodriguez].each do |other_user|
+    [phillip_fry, turanga_leela, bender_rodriguez].each do |other_user|
       FactoryGirl.create(:channel, users: [other_user, user_myself])
     end
 
