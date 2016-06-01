a7a6131a6bae62f66340edd98b8548660795c219
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-01 17:48:06 -0400

    Add API to access current users channels
    [#120618109]

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,11 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @channels = current_user.channels.includes(channels_users: :user)
 
+    respond_to do |format|
+      format.html
+      format.json { render json: @channels.to_json(include: { channels_users: { include: :user } }) }
+    end
   end
 end
