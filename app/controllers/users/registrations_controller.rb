2e8538ff10f59e0b4c2775147e3d23d35a4471ab
Jimmy Chao and Oana Tararache <pair+jchao+oana@casecommons.org>
2016-06-01 14:26:06 -0400

    Call set_minimum_password_length in edit
    to display minimum_password_length label text in edit view
    
    [#120483327]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def edit
+    set_minimum_password_length
+    super
+  end
+
   protected
   def update_resource(resource, update_account_params)
     if updating_password?(update_account_params)
