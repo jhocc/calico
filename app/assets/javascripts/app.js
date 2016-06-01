ed5806d61befbc6cfffdf186074b872ff4ecedb4
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-01 10:00:36 -0400

    Add data presents to app js
    This allows us to dynamically load react components
    on pages.
    
    [#120618109]

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,3 +5,20 @@ import ReactDOM from 'react-dom'
 import rails from 'jquery-ujs'
 import 'vendor/drawer'
 import 'menu'
+import 'presenter'
+import _ from 'lodash'
+
+function bindAndRenderReact(eventName, Component, dataAttrs = [], dataTransformer = (x) => x) {
+  $(document).bind(eventName, (event) => {
+    const el = event.target
+    const props = dataTransformer(_.pick($(el).data(), dataAttrs))
+    ReactDOM.render(<Component {...props} />, el)
+  })
+}
+
+function present() {
+  $(document).ready(function() {
+    window.present($('body'))
+  })
+}
+present()
