8b6ab1355d6ae7ef0b056a90fd98f8c929eb4ad0
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-01 17:43:44 -0400

    Generate Name in User factory
    This allows users to be more distinguishable
    in test scenarios

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     password 'Password123'
     password_confirmation 'Password123'
     factory :user_with_addresses do
