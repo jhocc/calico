000f33a71ffbfbd45d5ee9b09c002f381afd4e80
Pema Geyleg <pair+pema@casecommons.org>
2016-06-02 14:16:37 -0400

    Clean up puma configuration
    [#120132823]

--- config/puma.rb
+++ config/puma.rb
@@ -8,12 +8,6 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-app_dir = File.expand_path("../..", __FILE__)
-tmp_dir = "#{app_dir}/tmp"
-bind "unix://#{tmp_dir}/sockets/puma.sock"
-pidfile "#{tmp_dir}/pids/puma.pid"
-state_path "#{tmp_dir}/pids/puma.state"
-
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
