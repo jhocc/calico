6793e29c7b3dc7239ee4a5b45883fdce75e966c2
Jimmy Chao and Oana Tararache <pair+jchao+oana@casecommons.org>
2016-06-02 10:24:25 -0400

    Require drawer and menu code in app.js
    [#120375747]

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,7 +1,9 @@
-$('html').click(function() {
-  $('.drawer').removeClass('open')
-});
+$.ready(() => {
+  $('html').click(function() {
+    $('.drawer').removeClass('open')
+  });
 
-$('.drawer').click(function(event){
+  $('.drawer').click(function(event){
     event.stopPropagation();
-});
+  });
+})
