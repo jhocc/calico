0a8beb403929e143bc5d85247eb7a8ccf783f8a3
Pema Geyleg <pair+pema@casecommons.org>
2016-06-03 11:14:34 -0400

    Rename RAILS_MAX_THREADS to MAX_THREADS
    [#120132823]

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
@@ -9,9 +9,7 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
+  ActiveRecord::Base.establish_connection
 end
 
 before_fork do
