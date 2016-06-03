0c1782aac51ac729e1a6e1b3a13411edcf1020e5
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-03 14:01:29 -0400

    Add style changes to channel nav
    [#120608551]

--- app/assets/javascripts/components/ChannelNav.jsx
+++ app/assets/javascripts/components/ChannelNav.jsx
@@ -5,24 +5,29 @@ export default class ChannelNav extends Component {
   render() {
     var divStyle = { background: 'white' }
     return (
-      <ul className='channels' style={divStyle}>
-        {
-          this.props.data.map((channel, index) => {
-            const user = channel.getIn(['channels_users', 0, 'user'])
-            const firstName = user.get('first_name')
-            const lastName = user.get('last_name')
-            const onChannelSelect = () => { this.props.onChannelSelect(index) }
-            return (
-              <li>
-                <a href='#' onClick={onChannelSelect}>
-                  <img src='' alt='' />
-                  <span>{firstName} {lastName}</span>
-                </a>
-              </li>
-              )
-            })
-        }
-      </ul>
+            <div>
+              <div className="sidebar-header"><i className="fa fa-comments" aria-hidden="true"></i>
+                My Conversations
+              </div>
+              <ul className='channels' style={divStyle}>
+                {
+                  this.props.data.map((channel, index) => {
+                    const user = channel.getIn(['channels_users', 0, 'user'])
+                    const firstName = user.get('first_name')
+                    const lastName = user.get('last_name')
+                    const onChannelSelect = () => { this.props.onChannelSelect(index) }
+                    return (
+                      <li>
+                        <a href='#' onClick={onChannelSelect}>
+                          <img src='' alt='' />
+                          <span>{firstName} {lastName}</span>
+                        </a>
+                      </li>
+                      )
+                  })
+                }
+              </ul>
+            </div>
     )
   }
 }
