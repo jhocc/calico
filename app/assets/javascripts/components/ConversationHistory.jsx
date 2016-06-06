5cd5fd7d93e4b0f8a9b1322c5e3b67977aa5a25e
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-06 14:40:02 -0400

    Add resource link to calico welcome message
    [#120535379]

--- app/assets/javascripts/components/ConversationHistory.jsx
+++ app/assets/javascripts/components/ConversationHistory.jsx
@@ -27,7 +27,7 @@ export default class ConversationHistory extends Component {
               <p>Hi there!<br /><br />
                 Welcome to Calico, a messaging app for caseworkers, birth and foster parents,
                 designed to help coordinate and communicate. <br /><br />
-                To get started, click on the menu button (<i className='fa fa-bars' aria-hidden='true'></i>) and go to the <a>Resource Finder</a> (<i className='fa fa-crosshairs' aria-hidden='true'></i>).
+                To get started, click on the menu button (<i className='fa fa-bars' aria-hidden='true'></i>) and go to the <a href='/foster_family_agencies'>Resource Finder</a> (<i className='fa fa-crosshairs' aria-hidden='true'></i>).
                 This will put you in touch with foster family agencies in your area.
                 From there, select a resource and you will see a list of caseworkers that you can message.<br /><br />
                 Next time you login, you will see all conversations with caseworkers on your home page so you can message there.</p>
