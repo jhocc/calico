fe420ff6ef51b8f9c92d82d2e9dc9e171dec220d
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-08 18:24:14 -0400

    Add helper image to public assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ help_user = User.find_or_initialize_by(
   user.password = 'Password123'
 end
 
-image_file = File.open(File.join(Rails.root, '/public/assets/helper.png'))
+image_file = File.open(File.join(Rails.root, '/public/assets/images/helper.png'))
 help_user.profile_photo.store!(image_file)
 help_user.save!
 
