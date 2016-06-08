f2e48cd40a4f45948729924499bebce0c1e51d12
Pema Geyleg <pair+pema@casecommons.org>
2016-06-08 12:13:01 -0400

    Production accepts requests over ssl
    [#121095781]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require 'gulp_assets'
 class ApplicationController < ActionController::Base
+  force_ssl if: :ssl_configured?
+
   include GulpAssets::Helper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,6 +9,10 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :json_request?
 
   helper_method :gulp_asset_path
+
+  def ssl_configured?
+    Rails.env.production?
+  end
 
   protected
 
