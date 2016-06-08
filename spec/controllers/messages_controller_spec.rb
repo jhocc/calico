f0fcc79fb244cc875260861f473c5dd3b5978993
Johny Ho and Nate Jones <pair+Johny+nate@casecommons.org>
2016-06-08 18:14:21 -0400

    Update channels updated_at upon message creation
    This will push channels with new messages to the top
    
    [#121093099]

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,24 +4,35 @@ RSpec.describe MessagesController, type: :controller do
   let(:current_user) { FactoryGirl.create(:user, first_name: 'Current', last_name: 'User') }
 
   describe '#create' do
-    before do
-      sign_in current_user
-    end
-
-    it 'creates a message with the current user in the selected channel' do
-      phillip_fry = FactoryGirl.build(
+    let(:phillip_fry) do
+      FactoryGirl.build(
         :user,
         first_name: 'Phillip',
         last_name: 'Fry',
         email: 'phillip.fry@futuramalabs.com'
       )
-      channel_one = FactoryGirl.create(:channel, users: [phillip_fry, current_user])
+    end
+    let!(:channel) { FactoryGirl.create(:channel, users: [phillip_fry, current_user]) }
+
+    before do
+      sign_in current_user
+    end
+
+    it 'creates a message with the current user in the selected channel' do
       expect {
-        post :create, { channel_id: channel_one.id, message: { content: 'hello' }, format: :json }
+        post :create, { channel_id: channel.id, message: { content: 'hello' }, format: :json }
       }.to change(Message, :count).by(1)
       expect(current_user.channels.first.messages.first.content).to eq 'hello'
       expect(current_user.channels.first.messages.first.user_id).to eq current_user.id
-      expect(current_user.channels.first.messages.first.channel_id).to eq channel_one.id
+      expect(current_user.channels.first.messages.first.channel_id).to eq channel.id
+    end
+
+    it 'updates the channels updated at' do
+      expect {
+        post :create, { channel_id: channel.id, message: { content: 'hello' }, format: :json }
+      }.to change {
+        channel.reload.updated_at
+      }
     end
   end
 end
